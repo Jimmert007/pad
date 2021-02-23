29be11db467f96a1b76feb5d526dea4f4238b58c
nielsduif <niels-duivenvoorden@hotmail.com>
2021-02-23 10:43:50 +0100

    add gamestates gameobject gameenvironment
    with cell example

--- Game1.cs
+++ Game1.cs
@@ -5,63 +5,16 @@ using System.Diagnostics;
 
 namespace BaseProject
 {
-    public class Game1 : Game
+    class Game1 : GameEnvironment
     {
-        private GraphicsDeviceManager _graphics;
-        SpriteBatch spriteBatch;
-
-        Cell cell;
-
-        private Cell _cell;
-        Texture2D background;
-
-        public Game1()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-        }
-
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-            cell = new Cell(10,10, this);
-
-            base.Initialize();
-        }
-
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            base.LoadContent();
+            screen = new Point(520, 300);
+            ApplyResolutionSettings();
 
-            // TODO: use this.Content to load your game content here
-            background = Content.Load<Texture2D>("test");
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-            spriteBatch.Begin();
-
-            //spriteBatch.Draw(background, new Rectangle(0, 0, 10, 10), Color.Black);
-
-            cell.Display();
-
-            spriteBatch.End();
-
-            base.Draw(gameTime);
+            gameStateList.Add(new PlayingState());
+            GameEnvironment.SwitchTo(0);
         }
     }
 }
---
+++ GameStates/PlayingState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace BaseProject
+{
+    class PlayingState : GameState
+    {
+        public PlayingState()
+        {
+            //gameObjectList.Add(new GameObject("spr_background"));  
+            gameObjectList.Add(new Cell("test", new Vector2(10, 10), new Vector2(0, 0)));
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
