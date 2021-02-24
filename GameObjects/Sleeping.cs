4ef9c730454b8354c206bb64af39d36c7b98b689
wessel vink <vinkwessel@hotmail.com>
2021-02-24 12:03:16 +0100

    Added FadeIn/Out when sleeping

--- GameObjects/Sleeping.cs
+++ GameObjects/Sleeping.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-
+using Microsoft.Xna.Framework.Graphics;
 namespace BaseProject
 {
     class Sleeping : GameObject
     {
-
-        public Sleeping(string _assetName) : base(_assetName) { }
-
         float energy = 50;
+        float fadeAmount = 0;
         bool useOnce = true;
-
+        bool fade = false;
+        bool fadeIn, fadeOut;
+        Color color1, color2, finalColor;
+        public Texture2D fadeSprite;
+        public Sleeping(string _assetName) : base(_assetName)
+        {
+            color1 = new Color(0,0,0,0);
+            color2 = new Color(0,0,0,255);
+            fadeSprite = GameEnvironment.ContentManager.Load<Texture2D>("test");
+            texture = fadeSprite;
+        }
 
         public void Update(GlobalTime globalTime)
         {
@@ -22,11 +31,47 @@ namespace BaseProject
                 Sleep(globalTime);
                 useOnce = false;
             }
+            if (fade)
+            {
+                FadeScreen();
+            }
+            
+            if(fadeAmount >= 1)
+            {
+                fadeIn = false;
+                fadeOut = true;
+            }
+            if(fadeAmount <= 0)
+            {
+                fadeOut = false;
+                useOnce = true;
+            }
         }
-
+     
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (fade)
+            {
+                spriteBatch.Draw(texture, new Rectangle(0, 0, GameEnvironment.Screen.X, GameEnvironment.Screen.Y), finalColor);
+            }
+            }
+        public void FadeScreen()
+        {
+            if (fadeIn)
+            {
+                fadeAmount += .01f;
+            }
+            else if (fadeOut)
+            {
+                fadeAmount -= 0.01f;
+            }
+            finalColor = Color.Lerp(color1, color2, fadeAmount);
+        }
 
         void Sleep(GlobalTime time)
         {
+            fade = true;
+            fadeIn = true;
             energy += 50;
             time.Reset();
             //plant grow
