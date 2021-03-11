321fd40a5eb9efedf1a095d9e95740e7e5373119
Luke Sikma <luke.sikma@hva.nl>
2021-03-11 10:20:01 +0100

    Hotbar & vakken

--- GameObjects/Hotbar.cs
+++ GameObjects/Hotbar.cs
@@ -10,22 +10,31 @@ namespace BaseProject
     class Hotbar : GameObject
     {
         public List<GameObject> hotbarItemList;
+        public Texture2D hotBarBackground;
         public int ScreenWidth;
         public int ScreenHeight;
+
+        public int HBWidth;
 
         public Hotbar(string _assetName) : base(_assetName)
         {
             hotbarItemList = new List<GameObject>();
+            hotBarBackground = GameEnvironment.ContentManager.Load<Texture2D>("1px");
 
             ScreenWidth = GameEnvironment.screen.X;
             ScreenHeight = GameEnvironment.screen.Y;
-            position.X = ScreenWidth / 2 - texture.Width / 2;
-           /* position.Y = ScreenHeight - texture.Height;*/
-
-            /* velocity = _velocity;*/
+            
+            HBWidth = 455;
+            size.X = HBWidth;
+            size.Y = 50;
+            position.X = ScreenWidth / 2 - HBWidth /2;
+            position.Y = ScreenHeight - size.Y; 
 
         }
-
-      
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(hotBarBackground, new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), Color.Black);
+            
+        }
     }
 }
