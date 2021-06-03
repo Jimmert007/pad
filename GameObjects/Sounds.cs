00aa7d204ead7e044a8fdf0a9d3bfa83b98ef7c0
nielsduif <niels-duivenvoorden@hotmail.com>
2021-06-03 10:32:34 +0200

    Update Sounds.cs

--- GameObjects/Sounds.cs
+++ GameObjects/Sounds.cs
@@ -8,12 +8,11 @@ namespace HarvestValley.GameObjects
     /// </summary>
     public class Sounds : GameObjectList
     {
-        //Array with al the soundeffect names
+        //Array with all the soundeffect names
         public string[] soundEffectStrings = { "FootstepsOnGrass", "AxeSwing", "PickaxeSwing", "TreeFalling", "WaterSplash", "RoosterCrowing", "MetalRattling", "HittingGround", "Shaking1", "ButtonClick", "WheatPickup", "CoinDrop", "PlacingTree" }; 
 
         public SoundEffect[] SFXs;
         public SoundEffectInstance[] SEIs; // A Array to use when wanted to play a sound
-
 
         public Sounds()
         {
@@ -30,5 +29,4 @@ namespace HarvestValley.GameObjects
             }
         }
     }
-
 }
