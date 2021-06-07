9fa0b5528fbaf184b91578a55f16717bc5c71aae
nielsduif <niels-duivenvoorden@hotmail.com>
2021-06-07 10:58:24 +0200

    comments mouse

--- GameObjects/MouseGameObject.cs
+++ GameObjects/MouseGameObject.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace HarvestValley.GameObjects
 {
+    /// <summary>
+    /// Niels Duivnevoorden
+    /// Class to draw a mouseobject
+    /// Also contains collision with one pixel in th top left corner of the object
+    /// </summary>
     class MouseGameObject : GameObjectList
     {
         SpriteGameObject mouseSprite, collideSprite;
@@ -17,15 +22,24 @@ namespace HarvestValley.GameObjects
         public override void HandleInput(InputHelper inputHelper)
         {
             base.HandleInput(inputHelper);
+            //Set position to the mouse position
             mouseSprite.Position = inputHelper.MousePosition;
             collideSprite.Position = inputHelper.MousePosition;
         }
 
+        /// <summary>
+        /// Check collision between the mouse and given SGO
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public bool CollidesWith(SpriteGameObject other)
         {
             return (collideSprite.CollidesWith(other));
         }
 
+        /// <summary>
+        /// Use this SGO to get the hitbox of the mouse
+        /// </summary>
         public SpriteGameObject HitBox
         {
             get { return collideSprite; }
